f09443a853a3b07a13f6fd4cf788d1efbaeb6dc5
furno west <furnowest57@gmail.com>
2020-11-08 21:11:27 -0800

    first commit

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt')
+const Users = require('../models/users')
 
-router.post('/register', (req, res) => {
+
+
+router.post('/register', async(req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password,10)
+  user.password= hash
+  Users.add(user)
+  .then((saved) =>{
+    res.status(201).json(saved)
+  })
+  .catch((err)=>{
+    res.status(500).json(err)
+  })
 });
 
 router.post('/login', (req, res) => {
